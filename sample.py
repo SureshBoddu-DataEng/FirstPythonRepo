ffeb3081c69a4bf13db62def4cd6aeda4aa03565
sureshboddu <sureshsurya595@example.com>
2024-12-15 13:52:37 -0600

    new comments

--- sample.py
+++ sample.py
@@ -2,3 +2,23 @@ def sum(a, b):
 	return a + b
 	
 print(sum(10,20))
+
+# git clone repository name
+
+# git pull
+
+# git add .
+# git commit -m 'first commit'
+# git push origin
+
+
+# git branch
+# git branch python
+
+
+# git checkout test
+
+
+# git status
+
+# git stash
