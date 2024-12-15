e7828e039221023a748eee431c34f2920091eabb
sureshboddu <sureshsurya595@example.com>
2024-12-15 14:31:42 -0600

    issue fix

--- sample1.py
+++ sample1.py
@@ -10,6 +10,9 @@ def multiply(a, b):
     return a * b
     
 def division(a, b):
-    return a/b
+    if b != 0:
+        return a/b
+    else:
+        return "Denominator should not be zero"
     
 print(division(10,0))
